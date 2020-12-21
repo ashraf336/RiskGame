f87e39a291d613875bcc38032f466e32dc27f26b
ahmed <ahmed99ashraf@gmail.com>
2020-12-21 19:35:31 +0200

    removing from player (index 1 only ) and adding it to player 2

--- Game/player.py
+++ Game/player.py
@@ -5,14 +5,17 @@ class player:
         self.type = type
 
     def add_terr(self, terr):
-        self.my_terrs.append()
+        self.my_terrs.append(terr)
 
 
     def remove_terr(self,id):
-        self.my_terrs.pop
-
+        removed_terr = None
+        for i in range (len(self.my_terrs)):
+            if self.my_terrs[i].terr_id == id:
+                removed_terr=self.my_terrs.pop(i)
+                return removed_terr
 
         return removed_terr
 
-    def get_terr(self,id):
-        for i in range (len(self.my_terrs)):
+# def get_terr(self,id):
+    #    for i in range (len(self.my_terrs)):
--- Game/territory.py
+++ Game/territory.py
@@ -2,6 +2,7 @@
 class terr:
     def __init__(self, terr_id, neighbours):
         self.terr_id = terr_id
-        self.neighbours = neighbours  #list
+        self.neighbours = neighbours#list
+        self.army = 0
     def add_neighbours(self,neighbours_list):
         self.neighbours=neighbours_list
